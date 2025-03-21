71e05729f0cadc2b86e56fec21f64a0fcf7259ff
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-03-21 14:16:54 -0400

    Clase 2

--- ToDoApp/src/app/app.component.ts
+++ ToDoApp/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'ToDoList';
+  welcome = '¡Bienvenido!'
+  taskList= [
+    'Instalar el Angular CLI',
+    'Crear un nuevo proyecto con Angular CLI',
+    'Crear compontentes',
+  ]
 }
