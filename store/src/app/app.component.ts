743cdbaa61056cb86a0792f3870f82844523f2ac
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-04-07 16:07:22 -0400

    [clase 29] Componentes y templates en Angular

--- store/src/app/app.component.ts
+++ store/src/app/app.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
+@Component({ // Normalmente en el decorador se importa un css y un html, 
+// pero como en el html es solo una linea de codigo, lo podemos colocar directamente como "template" 
+// y podemos borrar el archivo html y css si no es necesario
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet],
+  template: '<router-outlet />', // Cambiamos templateURL a template, ya que es pequeño el archivo.
+  // En template colocamos <router-outlet/>
+  // Borrarmos el styleURL ya que no es necesario
 })
+
 export class AppComponent {
   title = 'store';
 }
