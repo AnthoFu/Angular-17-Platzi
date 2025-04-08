81b92415a2dc55a9e954d19629c7e3a99a855d05
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-04-07 22:20:42 -0400

    [Clase 36] ngOnChanges

--- store/src/app/domains/info/pages/about/about.component.ts
+++ store/src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , signal} from '@angular/core';
+import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CounterComponent, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  duration= signal(1000);
+  message= signal('Hola');
 
+
+  changeDuration(event:Event){
+    const input = event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber);
+  }
+
+
+  
+  changeMessage(event:Event){
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value);
+  }
 }
