bb8547434ca98cfc94f6589ee2b4e6f44d57348d
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-04-10 13:30:30 -0400

    [Clase 42 en proceso]

--- store/src/app/domains/products/pages/list/list.component.ts
+++ store/src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { product } from '../../../shared/components/models/product.model'
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,27 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  
+  products = signal<product[]>([]);
+
+  constructor(){
+    const initProduct: product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 10.99,
+        image: 'https://picsum.photos/200/300',
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 9.99,
+        image: 'https://picsum.photos/200/301',
+      }
+    ]
+    this.products.set(initProduct);
+  }
+
   getDataFromProduct(event:string){
     console.log('[Componente padre]:', event)
   }
