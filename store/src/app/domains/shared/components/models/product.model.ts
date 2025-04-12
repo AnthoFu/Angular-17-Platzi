e46907daadb5f70353af5a27ead5b755b78ed84a
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-04-12 15:04:47 -0400

    [Personalizacion de E-Commerce 1]

--- store/src/app/domains/shared/components/models/product.model.ts
+++ store/src/app/domains/shared/components/models/product.model.ts
@@ -1,12 +1,14 @@
 export interface product{
     id: number,
     title: string,
+    slug: string,
     price: number,
+    description: string,
     category:{
         id: number,
         name: string,
         image: string,
     }
+    
     images: string [],
-    creationAt: string,
 }
