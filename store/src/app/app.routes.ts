6b24588030924d8f6ec9a8d448a196cb28721fb8
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-04-16 14:05:27 -0400

    [Clase 59] Vistas anidadas

--- store/src/app/app.routes.ts
+++ store/src/app/app.routes.ts
@@ -2,18 +2,27 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component'
 import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: ListComponent // [IMPORTANTE] Este es el elemento padre
-    },
 
     {
-        path: 'about',
-        component: AboutComponent // Componente para testeos
+        path: '', // Este es el path principal, revisar componente [layoutcomponent]
+        component: LayoutComponent,
+        children:[
+            {
+                path: '',
+                component: ListComponent // Este es el primer hijo que se renderizara.
+            },
+        
+            {
+                path: 'about',
+                component: AboutComponent // Componente para testeos
+            },
+        
+        ]
     },
-
+    
 
 
 
