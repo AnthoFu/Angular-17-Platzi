1e00332a414cb218486fcde78d9d4539e5bfa545
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-04-21 09:39:24 -0400

    [Clase 69] Aplicando el Prefetching

--- store/src/app/app.config.ts
+++ store/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 
@@ -8,6 +8,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()), // Importamos la funcion withComponentInputBinding() para que los componentes puedan obtener parametros como inputs a las paginas
+    provideRouter(routes, 
+      withComponentInputBinding(), // Importamos la funcion withComponentInputBinding() para que los componentes puedan obtener parametros como inputs a las paginas 
+      withPreloading(PreloadAllModules)), // Configuramos la precarga de todos los módulos, esto permite que los módulos se carguen en segundo plano para mejorar el rendimiento de la aplicación  
     provideHttpClient(),]
   };
